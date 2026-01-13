0b7cb218a777df1fdf53c5656a8a71ca4d07499a
taufan12345 <abaysurbakti17@gmail.com>
2026-01-13 22:07:59 +0700

    add splash Screen

---
+++ app/src/main/java/com/taufan/projectakhir/SplashScreen.kt
@@ -0,0 +1,43 @@
+package com.taufan.projectakhir
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.widget.ImageView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash_screen)
+        
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val logo = findViewById<ImageView>(R.id.iv_logo)
+
+        // Animasi muncul dari tengah (scale up) selama 100ms
+        logo.scaleX = 0f
+        logo.scaleY = 0f
+        logo.animate()
+            .scaleX(1f)
+            .scaleY(1f)
+            .setDuration(100)
+            .start()
+
+        // Pindah ke MainActivity setelah 2 detik
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 2000)
+    }
+}
